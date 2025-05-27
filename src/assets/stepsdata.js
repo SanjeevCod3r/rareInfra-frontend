7ffc85c2b95c87671f923b71c9e7648ae1fda71e
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-05-27 16:42:37 +0530

    [Sanjeev Jaiswal] Add: hww Change Done

--- src/assets/stepsdata.js
+++ src/assets/stepsdata.js
@@ -21,4 +21,9 @@ export const steps = [
     title: 'Close the Deal',
     description: 'Complete paperwork digitally, get expert guidance from our agents, and secure your dream property with ease.',
   },
+  {
+    icon: FileCheck,
+    title: 'Close the Deal',
+    description: 'Complete paperwork digitally, get expert guidance from our agents, and secure your dream property with ease.',
+  },
 ];
