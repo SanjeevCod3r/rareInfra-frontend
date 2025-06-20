36726b29633f4a6b9de6386d1775304b5846da49
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-06-20 22:34:26 +0530

    [Sanjeev Jaiswal] Add: Done

--- src/assets/testimonialdata.js
+++ src/assets/testimonialdata.js
@@ -12,7 +12,7 @@ export const testimonials = [
     location: "Educator in Unacademy",
     image: abhijeetimage,
     rating: 5,
-    text: "RTN RareInfra made buying my home smooth and hassle-free. Their team was always professional, responsive, and clear in communication. I felt supported throughout the process and highly recommend their reliable real estate services to others."
+    text: "RareInfra made buying my home smooth and hassle-free. Their team was always professional, responsive, and clear in communication. I felt supported throughout the process and highly recommend their reliable real estate services to others."
   },
   {
     id: 2,
@@ -20,7 +20,7 @@ export const testimonials = [
     location: "Senior Software Engineer (Rsystem)",
     image: vivekimage,
     rating: 5,
-    text: "I worked with Varsha during my commercial property purchase. She was professional, timely, and detail-oriented. Her guidance helped me secure a great deal. RTN RareInfra’s dedication to client satisfaction truly sets them apart in the real estate industry."
+    text: "I worked with Varsha during my commercial property purchase. She was professional, timely, and detail-oriented. Her guidance helped me secure a great deal. RareInfra’s dedication to client satisfaction truly sets them apart in the real estate industry."
   },
   {
     id: 3,
@@ -28,7 +28,7 @@ export const testimonials = [
     location: "An Army Officer",
     image: anandimage,
     rating: 4,
-    text: "RTN RareInfra understood my needs from the start. They showed me only relevant properties and guided me through every detail. The process was efficient, and their professionalism stood out. I’m very satisfied with the service and final purchase."
+    text: "RareInfra understood my needs from the start. They showed me only relevant properties and guided me through every detail. The process was efficient, and their professionalism stood out. I’m very satisfied with the service and final purchase."
   },
   {
     id: 4,
@@ -36,7 +36,7 @@ export const testimonials = [
     location: "India Today",
     image: vishalimage,
     rating: 5,
-    text: "As an out-of-state buyer, I appreciated RTN RareInfra’s virtual tours and updates. They kept everything organized and transparent. Their team made me feel supported throughout the process. I felt confident every step of the way, thanks to their expertise."
+    text: "As an out-of-state buyer, I appreciated RareInfra’s virtual tours and updates. They kept everything organized and transparent. Their team made me feel supported throughout the process. I felt confident every step of the way, thanks to their expertise."
   },
   {
     id: 5,
@@ -44,7 +44,7 @@ export const testimonials = [
     location: "Noida, UP",
     image: vamikaimage,
     rating: 5,
-    text: "The level of transparency I received from RTN RareInfra was impressive. From pricing to paperwork, everything was clearly explained. Their team was always available to assist and answer questions. I’m very satisfied and would work with them again confidently."
+    text: "The level of transparency I received from RareInfra was impressive. From pricing to paperwork, everything was clearly explained. Their team was always available to assist and answer questions. I’m very satisfied and would work with them again confidently."
   },
   {
     id: 6,
@@ -52,6 +52,6 @@ export const testimonials = [
     location: "Delhi, IN",
     image: naipalimage,
     rating: 4,
-    text: "RTN RareInfra provided excellent service while helping me find my home. They were responsive, professional, and truly understood my preferences. I was able to get a great property within budget. I highly recommend their trustworthy and knowledgeable real estate team."
+    text: "RareInfra provided excellent service while helping me find my home. They were responsive, professional, and truly understood my preferences. I was able to get a great property within budget. I highly recommend their trustworthy and knowledgeable real estate team."
   }
 ];
